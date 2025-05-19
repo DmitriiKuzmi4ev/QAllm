bfe8b5c2f40385880b94d7f25b346055ba2a0a07
dgkyzmichev <DGKuzmichev@alfabank.ru>
2025-05-19 12:21:24 +0300

    - fix design

--- src/main/java/org/example/controller/TestRequest.java
+++ src/main/java/org/example/controller/TestRequest.java
@@ -1,18 +1,41 @@
 package org.example.controller;
 
-
-import lombok.*;
-
-@Setter
-@Getter
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class TestRequest {
 
     private String prompt;
     private String testType;
     private String framework;
     private String apiFramework;
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public String getTestType() {
+        return testType;
+    }
+
+    public String getFramework() {
+        return framework;
+    }
+
+    public String getApiFramework() {
+        return apiFramework;
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+
+    public void setTestType(String testType) {
+        this.testType = testType;
+    }
+
+    public void setFramework(String framework) {
+        this.framework = framework;
+    }
+
+    public void setApiFramework(String apiFramework) {
+        this.apiFramework = apiFramework;
+    }
 }
